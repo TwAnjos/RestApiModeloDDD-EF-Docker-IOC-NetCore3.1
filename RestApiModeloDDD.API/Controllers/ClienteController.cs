a2e9cf836173575ce76897c00d84317104b98956
Thiago Anjos <thiagowaltrick@gmail.com>
2021-07-05 21:36:38 -0300

    Api RES DDD Swagger and SQLServer and Docker implemented

---
+++ RestApiModeloDDD.API/Controllers/ClienteController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiModeloDDD.Application.DTOs;
+using RestApiModeloDDD.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        private readonly IApplicationServiceCliente applicationServiceCliente;
+
+        public ClienteController(IApplicationServiceCliente applicationServiceCliente)
+        {
+            this.applicationServiceCliente = applicationServiceCliente;
+        }
+
+        /// <summary>
+        /// GetAll
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Clientes")]
+        public ActionResult<IEnumerable<ClienteDTO>> Clientes()
+        {
+            return Ok(applicationServiceCliente.GetAll());
+        }
+
+        /// <summary>
+        /// GetById
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<string> GetById(int id)
+        {
+            return Ok(applicationServiceCliente.GetById(id));
+        }
+
+        /// <summary>
+        /// PostClient
+        /// </summary>
+        /// <param name="clienteDTO"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult PostClient([FromBody] ClienteDTO clienteDTO)
+        {
+            try
+            {
+                if (clienteDTO is null)
+                {
+                    return NotFound();
+                }
+                applicationServiceCliente.Add(clienteDTO);
+                return Ok("Cliente Cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// PutClient
+        /// </summary>
+        /// <param name="clienteDTO"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public ActionResult PutClient([FromBody] ClienteDTO clienteDTO)
+        {
+            try
+            {
+                if (clienteDTO is null) return NotFound();
+
+                applicationServiceCliente.Update(clienteDTO);
+                return Ok("Cliente atualizado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="clienteDTO"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public ActionResult Delete([FromBody] ClienteDTO clienteDTO)
+        {
+            try
+            {
+                if (clienteDTO is null) return NotFound();
+
+                applicationServiceCliente.Remove(clienteDTO);
+
+                return Ok("Cliente Removido com sucesso!");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult TesteKey([FromHeader] string key)
+        {
+            return Ok("key = " + key);
+        }
+    }
+}
---
+++ RestApiModeloDDD.API/Controllers/ProdutoController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiModeloDDD.Application.DTOs;
+using RestApiModeloDDD.Application.Interfaces;
+using System;
+
+namespace RestApiModeloDDD.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutoController : Controller
+    {
+        private readonly IApplicationServiceProduto applicationServiceProduto;
+
+        public ProdutoController(IApplicationServiceProduto applicationServiceProduto)
+        {
+            this.applicationServiceProduto = applicationServiceProduto;
+        }
+
+        /// <summary>
+        /// GetById
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<string> GetById(int id)
+        {
+            return Ok(applicationServiceProduto.GetById(id));
+        }
+
+        /// <summary>
+        /// Post
+        /// </summary>
+        /// <param name="clienteDTO"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Post([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO is null)
+                {
+                    return NotFound();
+                }
+                applicationServiceProduto.Add(produtoDTO);
+                return Ok("produtoDTO Cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Put
+        /// </summary>
+        /// <param name="produtoDTO"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public ActionResult Put([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO is null) return NotFound();
+
+                applicationServiceProduto.Update(produtoDTO);
+                return Ok("Cliente atualizado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="clienteDTO"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public ActionResult Delete([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO is null) return NotFound();
+
+                applicationServiceProduto.Remove(produtoDTO);
+
+                return Ok("Cliente Removido com sucesso!");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Key([FromHeader] string key)
+        {
+            return Ok("key = " + key);
+        }
+    }
+}
